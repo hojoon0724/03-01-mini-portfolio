f801d55daf2cb6c355a5ce095b2cc6f579905cf4
Hojoon Kim <hojoon0724@gmail.com>
2024-01-26 17:59:39 -0800

    Added project placeholders, listed css elements

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,60 @@
 function Projects(props) {
-  return <div className="projects"></div>;
+  return (
+    <div className="projects-container">
+      <div className="project">
+        <div className="project-photo">
+          <img
+            src=""
+            alt=""
+          />
+        </div>
+        <div className="project-text-container">
+          <div className="project-title">Space News API</div>
+          <div className="project-description">
+            A website that fetches data from an API and renders the news with a short summary and a link to the article
+          </div>
+          <div className="project-links">
+            <a href="https://github.com/hojoon0724/seal_project1">GitHub</a>
+          </div>
+        </div>
+      </div>
+      <div className="project">
+        <div className="project-photo">
+          <img
+            src=""
+            alt=""
+          />
+        </div>
+        <div className="project-text-container">
+          <div className="project-title">Urban Travel Log</div>
+          <div className="project-description">
+            An app where the user can create a list of cities they travelled and wish to travel using MongoDB
+          </div>
+          <div className="project-links">
+            <a href="https://github.com/hojoon0724/seal_project2">GitHub</a>
+            <a href="https://hk-seal-project2.onrender.com/">Deployed</a>
+          </div>
+        </div>
+      </div>
+      <div className="project">
+        <div className="project-photo">
+          <img
+            src=""
+            alt=""
+          />
+        </div>
+        <div className="project-text-container">
+          <div className="project-title">Blackjack Card Counting Trainer</div>
+          <div className="project-description">
+            A tool that lets you play Blackjack and counts cards, allowing the user to train themselves
+          </div>
+          <div className="project-links">
+            <a href="https://github.com/hojoon0724/blackjack">GitHub</a>
+            <a href="https://hk-seal-project2.onrender.com/">Deployed</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
-export default Projects;
+export default Projects
