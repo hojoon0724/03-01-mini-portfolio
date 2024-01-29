9ace40ad9844b0146213bf699a5cf78269df805d
Hojoon Kim <hojoon0724@gmail.com>
2024-01-29 14:21:03 -0800

    more css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 function Header(props) {
   return (
     <div className="header">
-      <div className="left">Hojoon Kim</div>
-      <div className="right">
+      <div className="header-left">Hojoon Kim</div>
+      <div className="header-right">
         <a href={props.github}>GitHub</a>
         <a href={props.linkedin}>LinkedIn</a>
       </div>
     </div>
-  );
+  )
 }
-export default Header;
+export default Header
